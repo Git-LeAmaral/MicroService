220e89df74fee1720713757edda6e0ae8df60737
Leandro Amaral <lramaral02m@gmail.com>
2024-05-08 01:00:57 -0300

    created classDto, responseDto and autoMapper

---
+++ Microservices.Service.CouponAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microservices.Service.CouponAPI.Models;
+using Microservices.Service.CouponAPI.Models.Dto;
+
+namespace Microservices.Service.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
