e066060a81fae5c7d3807e721d21022e2595af46
Leandro Amaral <lramaral02m@gmail.com>
2024-05-09 00:24:49 -0300

    Register services in Program class file

--- Microservice.Web/Program.cs
+++ Microservice.Web/Program.cs
@@ -1,7 +1,19 @@
+using Microservice.Web.Service;
+using Microservice.Web.Service.IService;
+using Microservice.Web.Utility;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<ICouponService, CouponService>();
+
+SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
+
+builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
 
 var app = builder.Build();
 
