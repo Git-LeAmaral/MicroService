a64468de5c115cd99f4aea300cb33247939d11f0
Leandro Amaral <lramaral02m@gmail.com>
2024-05-10 14:12:34 -0300

    Created user and password authentication

---
+++ Microseervices.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Microseervices.Services.AuthAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Microseervices.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Microseervices.Services.AuthAPI.Models.Dto;
+
+namespace Microseervices.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<string> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
